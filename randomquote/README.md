d57647a70d8b82a0a6aa24721a3743f6931b0ece
Nick Whaley <nicholasrwhaley@gmail.com>
2019-10-22 11:16:03 -0700

    modify transpile package and add random quote list

--- randomquote/README.md
+++ randomquote/README.md
@@ -6,6 +6,9 @@ In the project directory, you can run:
 
 ### `npm start`
 
+First run nvm install stable to ensure compatible version
+of Node is running.
+
 Runs the app in the development mode.<br />
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
--- randomquote/src/App.jsx
+++ randomquote/src/App.jsx
@@ -6,20 +6,39 @@ class App extends React.Component {
     constructor(props){
       super(props)
 
-
+      this.state = {
+        quotes = [
+          {quote: 'We have nothing to fear, but fear itself.',
+          author: 'Franklin D. Roosevelt'},
+          {quote: 'We have done the impossible, and that makes us mighty.',
+          author: 'Joss Whedon'},
+          {quote: 'Justice rains from above!',
+          author: 'Pharah'},
+          {quote: 'Battle not with monsters lest ye become a monster',
+          author: 'Friedrich Nietsche'},
+          {quote: 'Nobody\'s perfect, I gotta work it again and again til I get it right',
+          author: 'Miley Cyrus'}
+        ]
+      }
 
 
     }
 
+
+    handleClick(){
+      console.log('hi')
+    }
+
     render(){
       return (
-        <div id="quote-box">
-          <p id="text">hi</p>
-          <p id="author">hi</p>
-          <button id="new-quote">New Quote</button>
-          <a id="tweet-quote" >hello</a>
 
+        <div id="quote-box">
+          <p id="text">quote</p>
+          <p id="author">author</p>
+          <button id="new-quote">New Quote</button>
+          <a id="tweet-quote" onClick={this.handleClick}>Tweet Quote</a>
         </div>
+
       )
     }
 
